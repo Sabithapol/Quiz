8ff8cc3cf735532bcacdcb322d9732e7c76c3de9
Sabithapol <Sabitha.poladi10@gmail.com>
2020-06-29 14:14:01 -0700

    added part 1

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,20 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
-    client: 'sqlite3',
+    client: "pg",
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
+      database: "dbquiz",
+      // username: 'your postgres username'
+      // password: 'your username password'
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      tablename: "migrations",
+      directory: "./db/migrations",
+    },
+
+    seeds: {
+      directory: "./db/seeds",
+    },
   },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
 };
