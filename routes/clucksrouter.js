2b1f0e5018982f987e80f0eda167bb9b56ab7dc9
Sabithapol <Sabitha.poladi10@gmail.com>
2020-07-09 16:48:04 -0700

    made edits1

--- routes/clucksrouter.js
+++ routes/clucksrouter.js
@@ -2,16 +2,20 @@ const express = require("express");
 const { response } = require("express");
 const router = express.Router();
 
-router.get("/Cluck", (req,res) => {
+router.get("/", (req,res) => {
     res.render("index")
   });
-  router.get("/new",(req,res)=>{
+router.get("/new",(req,res)=>{
     res.render("newcluck")
 });
 
 router.post("/", (req,res) => {
-    res.send("my form sends a post request to the action/root and i have to handle creating and inserting data ad redirect")   
+  const username = req.cookies.username
+  const content = req.body.content
+  const url = req.body.url
+  res.send("")   
 });
 
+
+
 module.exports = router;
-//bfbfbfbfbf
