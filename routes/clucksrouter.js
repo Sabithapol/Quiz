d0318c528da59f311092cf9ccceb0427cf641ce3
Sabithapol <Sabitha.poladi10@gmail.com>
2020-07-09 15:14:27 -0700

    made edits

--- routes/clucksrouter.js
+++ routes/clucksrouter.js
@@ -8,9 +8,10 @@ router.get("/Cluck", (req,res) => {
   router.get("/new",(req,res)=>{
     res.render("newcluck")
 });
+
 router.post("/", (req,res) => {
     res.send("my form sends a post request to the action/root and i have to handle creating and inserting data ad redirect")   
 });
 
-
 module.exports = router;
+//bfbfbfbfbf
